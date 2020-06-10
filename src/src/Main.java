6f079eba521fdeafa33fc87446ee879820410fab
Daniel Garcia <demonpogp@gmail.com>
2020-06-10 17:49:04 -0500

    agregando do while

--- src/src/Main.java
+++ src/src/Main.java
@@ -8,32 +8,37 @@ class Main {
 
 
     static public void triangleProblem(){
+        int a,b,c;
+        boolean c1,c2,c3;
 
         //Step 1: get input
-        System.out.println("Enter 3 integers which are sides of a triangle");
-        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
-        System.out.print("Enter first side: ");
-        int a= sc.nextInt();
-        if(!((1<= a) && (a<= 200))){
-            System.out.println("Value of a is not in the range of permitted values");
-            return;
-        }
-        System.out.print("Enter second side: ");
-        int b= sc.nextInt();
-        if(!((1<= b) && (b<= 200))){
-            System.out.println("Value of a is not in the range of permitted values");
-            return;
-        }
-        System.out.print("Enter third side: ");
-        int c= sc.nextInt();
-        if(!((1<= c) && (c<= 200))){
-            System.out.println("Value of a is not in the range of permitted values");
-            return;
-        }
-        System.out.println("Side A is " +a);
-        System.out.println("Side B is " +b);
-        System.out.println("Side C is " +c);
+        do{
+            System.out.println("Enter 3 integers which are sides of a triangle");
+            Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
+            System.out.print("Enter first side: ");
+            a= sc.nextInt();
+            System.out.print("Enter second side: ");
+            b= sc.nextInt();
+            System.out.print("Enter third side: ");
+            c= sc.nextInt();
 
+            c1 = (1 <= a) && (a <= 200);
+            c2 = (1 <= b) && (b <= 200);
+            c3 = (1 <= c) && (c <= 200);
+            if(!c1){
+                System.out.println("Value of a is not in the range of permitted values");
+            }
+            if(!c2){
+                System.out.println("Value of a is not in the range of permitted values");
+            }
+            if(!c3){
+                System.out.println("Value of 1a is not in the range of permitted values");
+            }
+            System.out.println("Side A is " +a);
+            System.out.println("Side B is " +b);
+            System.out.println("Side C is " +c);
+        }while (!(c1 && c2 && c3));
+        
 
         // Step 2: Is a trinagle?
         boolean isTriangle;
