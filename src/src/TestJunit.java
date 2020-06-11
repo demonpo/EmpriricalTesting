1403fca437cae65865b612ff9900f7a62851208a
davnm5 <josdanei@espol.edu.ec>
2020-06-11 13:07:46 -0500

    [UPD] Test Triangle Type

--- src/src/TestJunit.java
+++ src/src/TestJunit.java
@@ -5,8 +5,11 @@ public class TestJunit {
    @Test
    public void testTipo() {
       Triangle triangle = new Triangle();
+       System.out.println("**Test 1**");
       assertEquals("Isoceles",triangle.triangleProblem(5,5,3));
+       System.out.println("**Test 2**");
       assertEquals("Equilateral",triangle.triangleProblem(5,5,5));
+       System.out.println("**Test 3**");
       assertEquals("Escalene",triangle.triangleProblem(1,2,3));
    
    }
